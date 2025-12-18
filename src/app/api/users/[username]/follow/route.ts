d241896c18cf9c045bcce6d457a1ab2362ad09bf
vicsory <jkaelaugustin@gmail.com>
2025-12-18 15:54:36 +0400

    PhotoUrl fix

--- src/app/api/users/[username]/follow/route.ts
+++ src/app/api/users/[username]/follow/route.ts
@@ -46,7 +46,7 @@ export async function POST(request: NextRequest, { params: { username } }: { par
             sender: {
                 username: verifiedToken.username,
                 name: verifiedToken.name,
-                photoUrl: verifiedToken.photoUrl,
+                photoUrl: verifiedToken.photoUrl  || "",
             },
             content: null,
         };
