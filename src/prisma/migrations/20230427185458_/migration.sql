c644351236c9a1ca67b8aff660f8c66036c780d0
vicsory <jkaelaugustin@gmail.com>
2025-12-19 13:11:23 +0400

    fix 5

--- src/prisma/migrations/20230427185458_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Tweet" ADD COLUMN     "photoUrl" TEXT;
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "photoUrl" TEXT;
--- src/prisma/migrations/20230511000542_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `text` on the `Notification` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Notification" DROP COLUMN "text";
